64324d58bf87253ae122977f70747754e4fac420
s279796 <71285202+s279796@users.noreply.github.com>
2021-03-18 12:41:46 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,54 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":return img`
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. . . . . . . . 
+. b b d d b b . 
+b 1 1 3 3 1 1 b 
+b 1 3 5 5 3 1 b 
+b d 3 5 5 3 d b 
+c 1 1 d d 1 1 c 
+c d 1 d d 1 d c 
+. c c 7 6 c c . 
+. . 6 7 6 . . . 
+. . 6 6 8 8 8 6 
+. . 6 8 7 7 7 6 
+. . 8 7 7 7 6 . 
+. . 8 8 8 6 . . 
+`;
+            case "image3":return img`
+. . 3 3 . . . . . 3 3 . . . . . 
+. . 3 3 3 . . . 3 3 3 . . . . . 
+. . . 3 3 3 . 3 3 3 . . . . . . 
+. . . . 3 3 . 3 3 . . . . . . . 
+. . . 5 f f 5 f f 5 5 . . . . . 
+. . . 5 f f 5 f f 5 5 5 5 . . . 
+. . 5 5 f f 5 f f 9 5 5 9 . . . 
+. f f 5 f f 5 f f f 5 5 f . . . 
+. . 5 5 f f 5 f f 9 5 5 9 . . . 
+. . . 5 f f 5 f f 5 5 5 . . . . 
+. . . 5 f f 5 f f 5 5 . . . . . 
+. . . f . . f . . . f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
